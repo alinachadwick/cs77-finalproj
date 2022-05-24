1c4c1532d7002e993d25295130e7791def9519b8
Charlie Baker <charles.d.baker.24@dartmouth.edu>
2022-05-24 12:12:58 -0400

    Update glUtil.js

--- glUtil.js
+++ glUtil.js
@@ -1,5 +1,31 @@
 
-function setupTask(taskFunction) {
+function setupTask(canvasID, taskFunction) {
+    var canvas = document.getElementById(canvasId);
+    if (!canvas) {
+        console.log("Could not find canvas with id", canvasId);
+        return;
+    }
+    
+    try {
+        var gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+    } catch (e) {}
+    if (!gl) {
+        console.log("Could not initialise WebGL");
+        return;
+    }
+    
+    var renderWidth, renderHeight;
+    function computeCanvasSize() {
+        renderWidth = Math.min(canvas.parentNode.clientWidth - 20, 820);
+        renderHeight = Math.floor(renderWidth*9.0/16.0);
+        canvas.width = renderWidth;
+        canvas.height = renderHeight;
+        gl.viewport(0, 0, renderWidth, renderHeight);
+    }
+    
+    window.addEventListener('resize', computeCanvasSize);
+    computeCanvasSize();
+
     var task = new taskFunction(gl);
 
     var mouseDown = false;
