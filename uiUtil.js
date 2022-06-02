75adf7d46f09ca32f486a2007090ad547e711a87
avidixit27 <avidixit27@gmail.com>
2022-06-02 15:50:03 -0400

    Implemented a GLSL window into our javascript code

--- uiUtil.js
+++ uiUtil.js
@@ -1,6 +1,5 @@
-
-Slider = function(targetId, minValue, maxValue, initialValue, hasLabel, callback, callbackArgs) {
-    var target = isString(targetId) ? document.getElementById(targetId) : targetId;
+Slider = function(targetId, minValue, maxValue, initialValue, hasLabel, callback) {
+    var target = document.getElementById(targetId);
     if (!target)
         return;
         
@@ -10,8 +9,6 @@ Slider = function(targetId, minValue, maxValue, initialValue, hasLabel, callback
     this.minValue = minValue;
     this.maxValue = maxValue;
     this.callback = callback;
-    this.callbackArgs = callbackArgs || [];
-    this.callbackArgs.push(initialValue);
     
     this.sliderBar = document.createElement("div");
     this.sliderBar.className = "slider-bar";
@@ -48,4 +45,32 @@ Slider = function(targetId, minValue, maxValue, initialValue, hasLabel, callback
 Slider.prototype.mouseMove = function(event) {
     var rect = this.sliderBackground.getBoundingClientRect();
     this.setPosition((event.clientX - rect.left)/(rect.right - rect.left));
+}
+
+Slider.prototype.setLabel = function(text) {
+    if (this.label)
+        this.label.textContent = text;
+}
+
+Slider.prototype.setValue = function(value) {
+    value = Math.min(this.maxValue, Math.max(this.minValue, value));
+    if (value != this.value) {
+        this.value = value;
+        var percentage = Math.max(Math.min(Math.floor(100.0*(value - this.minValue)/(this.maxValue - this.minValue)), 100.0), 0.0);
+        this.sliderHandle.style.left = this.sliderBar.style.width = percentage.toString() + "%";
+        
+        if (this.callback)
+            this.callback(value);
+    }
+}
+
+Slider.prototype.setPosition = function(position) {
+    this.setValue(Math.floor(this.minValue + position*(this.maxValue - this.minValue)));
+}
+
+Slider.prototype.show = function(show) {
+    var display = show ? "block" : "none";
+    this.sliderBackground.style.display = display;
+    if (this.label)
+        this.label.style.display = display;
 }
